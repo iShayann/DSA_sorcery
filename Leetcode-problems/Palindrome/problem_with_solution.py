4c41a813426c16c80731fa54938d452c641c5984
HarshvMahawar <114311884+HarshvMahawar@users.noreply.github.com>
2023-10-29 04:54:34 +0530

    Add palindrome problem

---
+++ Leetcode-problems/Palindrome/problem_with_solution.py
@@ -0,0 +1,36 @@
+#---PROBLEM---
+# Given an integer x, return true if x is a 
+# palindrome
+# , and false otherwise.
+
+ 
+
+# Example 1:
+
+# Input: x = 121
+# Output: true
+# Explanation: 121 reads as 121 from left to right and from right to left.
+# Example 2:
+
+# Input: x = -121
+# Output: false
+# Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+# Example 3:
+
+# Input: x = 10
+# Output: false
+# Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+#---SOLUTION---
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        copy = x
+        result = 0 
+        while x != 0:
+            rem = x%10
+            result = result*10 + rem
+            x //= 10
+        return copy == result
