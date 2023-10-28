76081139d989e6663aee72935d2f72bd1ba62569
HarshvMahawar <114311884+HarshvMahawar@users.noreply.github.com>
2023-10-29 05:03:57 +0530

    Add MergeTwoSortedArray Problem

---
+++ Leetcode-problems/MergeTwoSortedArrays/problem_with_solution.py
@@ -0,0 +1,33 @@
+# You are given the heads of two sorted linked lists list1 and list2.
+# Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+# Return the head of the merged linked list.
+
+# Example 1:
+# Input: list1 = [1,2,4], list2 = [1,3,4]
+# Output: [1,1,2,3,4,4]
+
+# Example 2:
+# Input: list1 = [], list2 = []
+# Output: []
+
+# Example 3:
+# Input: list1 = [], list2 = [0]
+# Output: [0]
+
+#---SOLUTION---
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        cur = dummy = ListNode()
+        while list1 and list2:               
+            if list1.val < list2.val:
+                cur.next = list1
+                list1, cur = list1.next, list1
+            else:
+                cur.next = list2
+                list2, cur = list2.next, list2
+                
+        if list1 or list2:
+            cur.next = list1 if list1 else list2
+            
+        return dummy.next
